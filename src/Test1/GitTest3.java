398c034fd6d1d8f50fb4c4089fdec07f594d8134
javed <javed@HarrisNaseh.hsd1.va.comcast.net>
2022-04-05 23:24:23 -0400

    committing Test2 to BranchX

---
+++ src/Test1/GitTest3.java
@@ -0,0 +1,25 @@
+package Test1;
+
+public class GitTest3 {
+
+	public static void main(String[] args) {
+		int a=10,b=20;
+		//int c=a+b;
+		
+		System.out.println("Before swapping values are..."+a+"  "+b);
+		
+		//Logic1 - Third variable
+		
+		int c=a;
+		a=b;
+		b=c;
+		System.out.println("After swapping values are..."+a+"  "+b);
+		
+
+	}
+
+
+
+	}
+
+
